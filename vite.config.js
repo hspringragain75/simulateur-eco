520ec96e5aa64d01a9ad0965680c817fec908322
HspringragainCFTC <hspringragain@cftc.fr>
2026-01-12 10:09:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simulateur-eco/'  
+  base: '/simulateur-eco/'  // ⚠️ Cette ligne doit être là
 })
